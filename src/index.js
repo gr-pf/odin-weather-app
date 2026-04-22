099f75c4e8c0e1c35cfa281c9164e59f781dee6d
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-04-22 18:06:47 +0200

    Add basic fetching

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./style.css";
+
+const API_KEY = "VXG7GBWM2JRTWQAQLNZTR3P8G";
+
+async function getWeather(city) {
+  const unitGroup = "metric";
+  const include = "days,hours,current";
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unitGroup}&include=${include}&key=${API_KEY}`;
+
+  const response = await fetch(url);
+  const json = await response.json();
+
+  console.log(json);
+}
+
+getWeather("london");
